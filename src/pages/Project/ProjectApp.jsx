aac917574383af8576196672059ecac82ac905fa
Priyanka@20067 <priyankam18042006@gmail.com>
2025-07-05 11:42:07 +0530

    remove some unwanted code

--- src/pages/Project/ProjectApp.jsx
+++ src/pages/Project/ProjectApp.jsx
@@ -84,86 +84,9 @@ function ProjectApp() {
                             <img src={proyectsImgApp(`./proyecto-app-17.png`)} alt="" className="projects__img" />
                         </a>
                     </div>
-                    <div className="projects__item">
-                        <a onClick={() => cambiarEstadoModal16(!estadoModal16)}>
-                            <img src={proyectsImgApp(`./proyecto-app-16.jpg`)} alt="" className="projects__img" />
-                        </a>
-                    </div>
-                    <div className="projects__item">
-                        <a onClick={() => cambiarEstadoModal15(!estadoModal15)}>
-                            <img src={proyectsImgApp(`./proyecto-app-15.jpg`)} alt="" className="projects__img" />
-                        </a>
-                    </div>
-                    <div className="projects__item">
-                        <a onClick={() => cambiarEstadoModal14(!estadoModal14)}>
-                            <img src={proyectsImgApp(`./proyecto-app-14.jpg`)} alt="" className="projects__img" />
-                        </a>
-                    </div>
-                    <div className="projects__item">
-                        <a onClick={() => cambiarEstadoModal13(!estadoModal13)}>
-                            <img src={proyectsImgApp(`./proyecto-app-13.jpg`)} alt="" className="projects__img" />
-                        </a>
-                    </div>
-                    <div className="projects__item">
-                        <a onClick={() => cambiarEstadoModal12(!estadoModal12)}>
-                            <img src={proyectsImgApp(`./proyecto-app-12.jpg`)} alt="" className="projects__img" />
-                        </a>
-                    </div>
-                    <div className="projects__item">
-                        <a onClick={() => cambiarEstadoModal11(!estadoModal11)}>
-                            <img src={proyectsImgApp(`./proyecto-app-11.jpg`)} alt="" className="projects__img" />
-                        </a>
-                    </div>
-                    <div className="projects__item">
-                        <a onClick={() => cambiarEstadoModal10(!estadoModal10)}>
-                            <img src={proyectsImgApp(`./proyecto-app-10.jpg`)} alt="" className="projects__img" />
-                        </a>
-                    </div>
-                    <div className="projects__item">
-                        <a onClick={() => cambiarEstadoModal9(!estadoModal9)}>
-                            <img src={proyectsImgApp(`./proyecto-app-9.jpg`)} alt="" className="projects__img" />
-                        </a>
-                    </div>
-                    <div className="projects__item">
-                        <a onClick={() => cambiarEstadoModal8(!estadoModal8)}>
-                            <img src={proyectsImgApp(`./proyecto-app-8.jpg`)} alt="" className="projects__img" />
-                        </a>
-                    </div>
-                    <div className="projects__item">
-                        <a onClick={() => cambiarEstadoModal7(!estadoModal7)}>
-                            <img src={proyectsImgApp(`./proyecto-app-7.jpg`)} alt="" className="projects__img" />
-                        </a>
-                    </div>
-                    <div className="projects__item">
-                        <a onClick={() => cambiarEstadoModal6(!estadoModal6)}>
-                            <img src={proyectsImgApp(`./proyecto-app-6.jpg`)} alt="" className="projects__img" />
-                        </a>
-                    </div>
-                    <div className="projects__item">
-                        <a onClick={() => cambiarEstadoModal5(!estadoModal5)}>
-                            <img src={proyectsImgApp(`./proyecto-app-5.jpg`)} alt="" className="projects__img" />
-                        </a>
-                    </div>
-                    <div className="projects__item">
-                        <a onClick={() => cambiarEstadoModal4(!estadoModal4)}>
-                            <img src={proyectsImgApp(`./proyecto-app-4.jpg`)} alt="" className="projects__img" />
-                        </a>
-                    </div>
-                    <div className="projects__item">
-                        <a onClick={() => cambiarEstadoModal3(!estadoModal3)}>
-                            <img src={proyectsImgApp(`./proyecto-app-3.jpg`)} alt="" className="projects__img" />
-                        </a>
-                    </div>
-                    <div className="projects__item">
-                        <a onClick={() => cambiarEstadoModal2(!estadoModal2)}>
-                            <img src={proyectsImgApp(`./proyecto-app-2.jpg`)} alt="" className="projects__img" />
-                        </a>
-                    </div>
-                    <div className="projects__item">
-                        <a onClick={() => cambiarEstadoModal1(!estadoModal1)}>
-                            <img src={proyectsImgApp(`./proyecto-app-1.jpg`)} alt="" className="projects__img" />
-                        </a>
-                    </div>
+                   
+                    
+                   
                 </section>
             </main>
             <Modal
